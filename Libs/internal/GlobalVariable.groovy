589cfa3ac20c44fbb819a078e3108649e9d33455
Mariano Laje <marianolaje@outlook.com>
2019-07-18 12:15:46 -0300

    First GIT for Katalon Studio Project

---
+++ Libs/internal/GlobalVariable.groovy
@@ -0,0 +1,48 @@
+package internal
+
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.main.TestCaseMain
+
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+public class GlobalVariable {
+     
+    /**
+     * <p></p>
+     */
+    public static Object g_access_token
+     
+    /**
+     * <p></p>
+     */
+    public static Object g_refresh_token
+     
+    /**
+     * <p></p>
+     */
+    public static Object g_servidor_oauth
+     
+    /**
+     * <p></p>
+     */
+    public static Object g_servidor_crm
+     
+
+    static {
+        try {
+            def selectedVariables = TestCaseMain.getGlobalVariables("default")
+			selectedVariables += TestCaseMain.getGlobalVariables(RunConfiguration.getExecutionProfile())
+            selectedVariables += RunConfiguration.getOverridingParameters()
+    
+            g_access_token = selectedVariables['g_access_token']
+            g_refresh_token = selectedVariables['g_refresh_token']
+            g_servidor_oauth = selectedVariables['g_servidor_oauth']
+            g_servidor_crm = selectedVariables['g_servidor_crm']
+            
+        } catch (Exception e) {
+            TestCaseMain.logGlobalVariableError(e)
+        }
+    }
+}
---
+++ Test Suites/WSInmuebles/TS inmueble (CUIT).ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS inmueble (CUIT)</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>47f693ea-f9ac-4682-b973-305d90e4b669</testSuiteGuid>
+   <testCaseLink>
+      <guid>afc1178d-16b4-446d-8d2f-f410e827dc53</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WSInmuebles/afip mi inmueble ok (CUIT)</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>6a5d4d58-faa3-4c14-acf5-507651dc06cf</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/WSInmuebles/Inmuebles</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>6a5d4d58-faa3-4c14-acf5-507651dc06cf</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Inmuebles</value>
+         <variableId>31bd30d4-96ac-45b1-8f43-d7cf9448246a</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>1b50e6a7-6183-49e6-a71e-5519e7625fab</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>d38813ac-04a7-43d8-8c97-69e8d64fe415</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
